d54ace6cca724a9523f7f631079fdca35e5bae0b
AnnieWebDev <komochek2013@gmail.com>
2020-09-17 19:20:47 +0300

    swiper

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var mySwiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 30,
+    slidesPerGroup: 2,
+     // Navigation arrows
+    navigation: {
+      nextEl: '.next-slide',
+      prevEl: '.prev-slide',
+    },
+  
+ 
+  })
